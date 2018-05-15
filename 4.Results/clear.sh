22f851dd92b310e852dd0f7b252d3113928f5d72
Cristian Jara-Figueroa <cristianjf@cristians-mbp.media.mit.edu>
2018-05-15 10:55:45 -0400

    updated clear.sh

--- 4.Results/clear.sh
+++ 4.Results/clear.sh
@@ -1,28 +1,19 @@
 rm -r Figure1
 rm -r Figure2
+rm -r Figure3
 mkdir Figure1
 mkdir Figure2
+mkdir Figure3
 mkdir Figure1/BR_Ind
 mkdir Figure1/BR_Occ
 mkdir Figure1/US_Field
 mkdir Figure1/US_Ind
 mkdir Figure1/US_Occ
 mkdir Figure1/US_Tech
-mkdir Figure2/BR_Ind
-mkdir Figure2/BR_Occ
-mkdir Figure2/US_Field
-mkdir Figure2/US_Ind
-mkdir Figure2/US_Occ
-mkdir Figure2/US_Tech
 mkdir Figure1/BR_Ind/SM
 mkdir Figure1/BR_Occ/SM
 mkdir Figure1/US_Field/SM
 mkdir Figure1/US_Ind/SM
 mkdir Figure1/US_Occ/SM
 mkdir Figure1/US_Tech/SM
-mkdir Figure2/BR_Ind/SM
-mkdir Figure2/BR_Occ/SM
-mkdir Figure2/US_Field/SM
-mkdir Figure2/US_Ind/SM
-mkdir Figure2/US_Occ/SM
-mkdir Figure2/US_Tech/SM
+mkdir Figure2/SM
